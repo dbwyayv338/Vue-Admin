a5a71ec188f46071f8b0e3be904ec9917b6bb782
wangxinpeng9527@gmail.com <wangxinpeng9527@gmail.com>
2023-10-20 17:37:57 +0800

    search

--- resources/js/Components/Base/NavBarSearch.vue
+++ resources/js/Components/Base/NavBarSearch.vue
@@ -1,13 +1,47 @@
 <script setup>
 import FormControl from '@/Components/Base/FormControl.vue'
+import {computed, reactive, ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
+
+const keyword = ref(null);
+
+const menus = computed(() => usePage().props.navigation.menu);
+
+const search = (k) => {
+    const menu = menus.value;
+    const result = [];
+    const searchMenu = (menu) => {
+        for (const item of menu) {
+            if (item.children) {
+                searchMenu(item.children);
+            } else {
+                if (item.link.toLowerCase().includes(k.toLowerCase())) {
+                    result.push(item);
+                }
+            }
+        }
+    };
+    searchMenu(menu);
+    return result;
+}
+
+const enter = () => {
+    const result = search(keyword.value);
+    if (result.length) {
+        window.location.href = result[0].link;
+    }
+    return false;
+}
 </script>
 
 <template>
-  <FormControl
-    ref="root"
-    placeholder="Search (ctrl+k)"
-    ctrl-k-focus
-    transparent
-    borderless
-  />
+    <form @submit.prevent="enter">
+        <FormControl
+            placeholder="Search nav (Ctrl+K)"
+            ctrl-k-focus
+            transparent
+            borderless
+            v-model="keyword"
+        />
+    </form>
 </template>
